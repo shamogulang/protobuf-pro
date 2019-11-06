25b6b219b23f7b698f7bb3b407aa12fa0111d41d
chenjianhui <chenjianhui@itouchtv.cn>
2019-11-06 11:54:11 +0800

    json格式化

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,7 @@
 import com.example.tutorial.AddressBookProtos;
+import com.google.protobuf.util.JsonFormat;
+
+import java.util.Arrays;
 
 /**
  * @author chenjianhui on 2019/11/05
@@ -12,6 +15,15 @@ public class Client {
                     .setId(11)
                     .setEmail("23333@qq.com").build();
 
+            //把 person消息类对象 序列化为 byte[]字节
+            byte[] byteArray1 = person.toByteArray();
+            //转成字节数组输出
+            System.out.println(Arrays.toString(byteArray1));
+            //反序列化
+            System.out.println(AddressBookProtos.Person.parseFrom(byteArray1));
+
+            //转成json//要引入gson-2.8.6.jar
+            System.out.println(JsonFormat.printer().print(person));
         }catch (Exception e){
 
         }
